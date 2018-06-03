dd7cef5ea58fc2fe27638fce5567ac718e740e4e
chff <chff@ua.pt>
2018-06-03 18:29:22 +0100

    some ITs

--- src/test/java/db/SeleniumIT.java
+++ src/test/java/db/SeleniumIT.java
@@ -1,6 +1,7 @@
 package db;
 
 import com.mycompany.gocommunity.DatabaseHandler;
+import java.util.Calendar;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import javax.persistence.EntityManager;
@@ -28,14 +29,16 @@ public class SeleniumIT {
 
   @Before
   public void setUp() throws Exception {
-    //System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe");
+    System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
     driver = new ChromeDriver();
     baseUrl = "https://www.katalon.com/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
-  public void testUntitledTestCase() throws Exception {
+  public void seleniumTest() throws Exception {
+    System.out.println("Selenium Test");
+    
     String uname;
     String pname;
     
@@ -44,39 +47,41 @@ public class SeleniumIT {
         pname = randomName();
     } while (db.apiGetUser(uname)!=null || db.apiGetProject(pname)!=null);
 
-    driver.get("http://localhost:8080/GoCommunity/");
+    driver.get("http://deti-tqs-05.ua.pt:8181/GoCommunity-1.0-SNAPSHOT/faces/api/data");
     driver.findElement(By.linkText("Sign Up")).click();
-    driver.findElement(By.name("j_idt7:j_idt9")).clear();
-    driver.findElement(By.name("j_idt7:j_idt9")).sendKeys("Selenium User");
-    driver.findElement(By.name("j_idt7:j_idt11")).clear();
-    driver.findElement(By.name("j_idt7:j_idt11")).sendKeys(uname);
-    driver.findElement(By.name("j_idt7:j_idt13")).clear();
-    driver.findElement(By.name("j_idt7:j_idt13")).sendKeys("password219380");
-    driver.findElement(By.name("j_idt7:j_idt15")).click();
+    driver.findElement(By.id("a:n")).clear();
+    driver.findElement(By.id("a:n")).sendKeys("Selenium User");
+    driver.findElement(By.id("a:un")).clear();
+    driver.findElement(By.id("a:un")).sendKeys(uname);
+    driver.findElement(By.id("a:p")).clear();
+    driver.findElement(By.id("a:p")).sendKeys("password219380");
+    driver.findElement(By.id("a:b")).click();
     driver.findElement(By.linkText("New Project")).click();
-    driver.findElement(By.name("j_idt7:j_idt9")).clear();
-    driver.findElement(By.name("j_idt7:j_idt9")).sendKeys(pname);
-    driver.findElement(By.name("j_idt7:j_idt11")).clear();
-    driver.findElement(By.name("j_idt7:j_idt11")).sendKeys("This is the project's description. It's not very big but it gets the job done.");
-    driver.findElement(By.name("j_idt7:j_idt13")).clear();
-    driver.findElement(By.name("j_idt7:j_idt13")).sendKeys("3500.75");
-    driver.findElement(By.name("j_idt7:j_idt15")).clear();
-    driver.findElement(By.name("j_idt7:j_idt15")).sendKeys("2018-12-12");
-    driver.findElement(By.name("j_idt7:j_idt18")).click();
+    driver.findElement(By.id("p:n")).clear();
+    driver.findElement(By.id("p:n")).sendKeys(pname);
+    driver.findElement(By.id("p:d")).clear();
+    driver.findElement(By.id("p:d")).sendKeys("This is the project's description. It's not very big but it gets the job done.");
+    driver.findElement(By.id("p:g")).clear();
+    driver.findElement(By.id("p:g")).sendKeys("3500.75");
+    driver.findElement(By.id("p:t")).clear();
+    int nextyear = Calendar.getInstance().get(Calendar.YEAR) + 1;
+    driver.findElement(By.id("p:t")).sendKeys(nextyear+"-06-06");
+    driver.findElement(By.id("p:b")).click();
     driver.findElement(By.linkText("Search Projects")).click();
-    driver.findElement(By.name("j_idt7:j_idt9")).click();
-    driver.findElement(By.name("j_idt7:j_idt9")).clear();
-    driver.findElement(By.name("j_idt7:j_idt9")).sendKeys(pname);
-    driver.findElement(By.name("j_idt7:j_idt11")).click();
-    driver.findElement(By.name("j_idt7:j_idt18")).click();
-    driver.findElement(By.id("j_idt11:mkey")).clear();
-    driver.findElement(By.id("j_idt11:mkey")).sendKeys("100.00");
-    driver.findElement(By.id("j_idt11:mtxt")).clear();
-    driver.findElement(By.id("j_idt11:mtxt")).sendKeys("A free t-shirt for every backer if we get this far.");
-    driver.findElement(By.id("j_idt11:madd")).click();
-    driver.findElement(By.id("j_idt11:cmttxt")).clear();
-    driver.findElement(By.id("j_idt11:cmttxt")).sendKeys("First comment!");
-    driver.findElement(By.id("j_idt11:cmtadd")).click();
+    driver.findElement(By.id("s:se")).click();
+    driver.findElement(By.id("s:se")).clear();
+    driver.findElement(By.id("s:se")).sendKeys(pname);
+    driver.findElement(By.id("s:b")).click();
+    driver.findElement(By.id("s:pr")).click();
+    driver.findElement(By.id("p:mkey")).clear();
+    driver.findElement(By.id("p:mkey")).sendKeys("100.00");
+    driver.findElement(By.id("p:mtxt")).clear();
+    driver.findElement(By.id("p:mtxt")).sendKeys("A free t-shirt for every backer if we get this far.");
+    driver.findElement(By.id("p:madd")).click();
+    driver.findElement(By.id("p:cmttxt")).clear();
+    driver.findElement(By.id("p:cmttxt")).sendKeys("First comment!");
+    driver.findElement(By.id("p:cmtadd")).click();
+
   }
 
   @After
@@ -94,7 +99,7 @@ public class SeleniumIT {
     
     private String randomName() {
         int type;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("Selenium-");
         
         for (int i=0;i<20;i++) {
             type = random.nextInt(3);
